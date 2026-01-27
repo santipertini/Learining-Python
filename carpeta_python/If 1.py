b8bdb6bf7b13c568daa1977fcf27a775a853e01c
BiagoDev <pertiniperto@gmail.com>
2026-01-27 17:57:24 -0300

    Update and rename Primero.py to If 1.py

---
+++ carpeta_python/If 1.py
@@ -0,0 +1,7 @@
+entrada= input("cual es la mejor planta?\n")
+if entrada == "espatifilo":
+    print("NO, quiero un GRAN espatifilo")
+elif entrada == "ESPATIFILO":
+    print("SEEEEEE PADREE")
+else:
+    print("Es espatifilo, no", entrada)
